bda359b040a5a3ed5da8d1bbf3a8178a730c0635
Aboubacar Doucoure <yelounak@gmail.com>
2023-12-02 19:43:41 +0100

    feat(module): v1.0

--- src/utils/adaptConfig/index.js
+++
@@ -1,9 +0,0 @@
-import basic from "./basic.js"
-import setConfigurations from "./setConfigurations/index.js"
-
-
-export default (props) => {
-  // const { servableEngineConfig, live = false } = props
-  basic(props)
-  setConfigurations(props)
-}
