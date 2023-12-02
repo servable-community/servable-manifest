8528bb79c3939fc4f3c8b8b31b4f102f7935b91b
Aboubacar Doucoure <yelounak@gmail.com>
2023-12-02 19:38:36 +0100

    fix(index): removed unused entries

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import launchServable from './launch/index.js'
-import computeSchema from './lib/schema/compute/index.js'
+
+// import computeSchema from './lib/schema/compute/index.js'
 // import validateProtocol from './lib/config/validate/protocol/index.js'
-import cleanProtocols from './utils/cleanProtocols.js'
+// import cleanProtocols from './utils/cleanProtocols.js'
 import extractProtocol from './manifest/extract/index.js'
 import accessManifest from './manifest/access/index.js'
 import * as ManifestEnums from './manifest/enums.js'
 import documentProtocol from './document/index.js'
 
 export {
-  launchServable,
-  computeSchema,
-  cleanProtocols,
+
+  // computeSchema,
+  // cleanProtocols,
   accessManifest,
   documentProtocol,
   extractProtocol,
